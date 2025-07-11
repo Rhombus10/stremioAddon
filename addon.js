8cd379d73bb2bd49881edaacf4ce8df3868815d0
Guru <gamingmincepie@gmail.com>
2025-07-11 11:10:53 +0100

    updated

--- addon.js
+++ addon.js
@@ -15,15 +15,36 @@ const manifest = {
 
 const dataset = {
 	"ustv-de1092e3-23c9-40b8-9cd1-c1c11bfb262a": [
-		{ name: "fred's version", type: "tv", url: "https://fl3.moveonjoy.com/CNN/index.m3u8" }]
+		{ name: "src1", type: "tv", url:''}]
 }
 
 
 const builder = new addonBuilder(manifest)
 
+async function update() {
+  await fetch("http://www.freeintertv.com/myAjax/get_item_m3u8/", {
+    method: "POST",
+    headers: {
+      "accept": "text/plain, */*; q=0.01",
+      "accept-language": "en-GB,en;q=0.8",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "sec-gpc": "1",
+      "x-requested-with": "XMLHttpRequest",
+      "cookie": "lang=english; lastnew=3324%26%26200; TVSESS=f3sbhipol0ec153sojm9dnis52"
+    },
+    body: "chname=Y25uX2xpdmU%3D&ch=http%3A%2F%2Fwww.freeintertv.com%2Fexternals%2Ftv-russia%2Fsmotret-tv3-online&html5=11"
+  })
+  .then(res => res.text())
+  .then(text => {
+    const match = text.match(/playlist\[0\]\['file'\]='(.*?)'/);
+    dataset["ustv-de1092e3-23c9-40b8-9cd1-c1c11bfb262a"].find(a => a.name=="src1").url = match ? match[1] : null;
+  });
+}
+
 builder.defineStreamHandler(function(args) {
     if (dataset[args.id]) {
-        return Promise.resolve({ streams: dataset[args.id] });
+      return update().then(() => ({ streams: dataset[args.id] }));
+
     } else {
         return Promise.resolve({ streams: [] });
     }
